c5e63119e10df691fa9fbbb85cf40ced8ab10b12
tab <trevorbehnke@gmail.com>
2023-02-21 15:59:35 -0800

    block explorer

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -124,4 +124,54 @@ Blockchain.prototype.chainIsValid = function (blockchain) {
   return validChain;
 };
 
+// Method to get block using hash in block explorer
+Blockchain.prototype.getBlock = function (blockHash) {
+  let correctBlock = null;
+  this.chain.forEach((block) => {
+    if (block.hash === blockHash) {
+      correctBlock = block;
+    }
+  });
+  return correctBlock;
+};
+
+// Method to get transaction using block explorer
+Blockchain.prototype.getTransaction = function (transactionId) {
+  let correctTransaction = null;
+  let correctBlock = null;
+  this.chain.forEach((block) => {
+    block.transactions.forEach((transaction) => {
+      if (transaction.transactionId === transactionId) {
+        correctTransaction = transaction;
+        correctBlock = block;
+      }
+    });
+  });
+
+  return { transaction: correctTransaction, block: correctBlock };
+};
+
+// Method to get all transactions associated with an address
+Blockchain.prototype.getAddressData = function (address) {
+  const addressTransactions = [];
+  this.chain.forEach((block) => {
+    block.transactions.forEach((transaction) => {
+      if (transaction.sender === address || transaction.recipient === address) {
+        addressTransactions.push(transaction);
+      }
+    });
+  });
+
+  let balance = 0;
+  addressTransactions.forEach((transaction) => {
+    if (transaction.recipient === address) balance += transaction.amount;
+    else if (transaction.sender === address) balance -= transaction.amount;
+  });
+
+  return {
+    addressTransactions: addressTransactions,
+    addressBalance: balance,
+  };
+};
+
 module.exports = Blockchain;
